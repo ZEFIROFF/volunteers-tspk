2c8b9d8f011a8795bc8b5b88089ff855b510e775
Денис Зефиров <55986656+d3pii@users.noreply.github.com>
2021-06-11 21:44:42 +0400

    add home page and add structure

---
+++ src/component/pages/Home/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from '../../../favicon.png'
+import s from './Header.module.css'
+
+const Header = () => {
+    return (
+        <div>
+            <nav className={s.nav}>
+                <div className={s.logo}><img src={logo} alt={logo}/></div>
+                <ul className={s.nav_middle}>
+                    <li><a href="">Главная</a></li>
+                    <li><a href="">Наши волонтеры</a></li>
+                    <li><a href="">Фотоальбом</a></li>
+                </ul>
+                <div className={s.signin}>Войти</div>
+            </nav>
+            <div className={s.title}>
+
+            </div>
+        </div>
+    );
+};
+
+export default Header;
