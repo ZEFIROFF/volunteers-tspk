918b06fc7d9bcd69a00806945e7ce9e231b45c6e
Денис Зефиров <55986656+d3pii@users.noreply.github.com>
2021-06-13 00:30:18 +0400

    add header (this is shit lol)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,29 @@
 import React from 'react';
 import logo from '../../favicon.png'
 import s from './Header.module.css'
+import emoji from '../../img/emoji.png'
 
 const Header = () => {
     return (
-        <div>
-            <nav className={s.nav}>
-                <div className={s.logo}><img src={logo} alt={logo}/></div>
-                <ul className={s.nav_middle}>
-                    <li><a href="">Главная</a></li>
-                    <li><a href="">Наши волонтеры</a></li>
-                    <li><a href="">Фотоальбом</a></li>
-                </ul>
-                <div className={s.signin}>Войти</div>
-            </nav>
-            <div className={s.title}>
-
+        <header>
+            <div className="container-xl">
+                <nav className="row justify-content-between align-items-center">
+                    <div className="col col-lg-4"><img src={logo} alt={logo}/></div>
+                    <ul className={s.navbar + " col col-md-7 row "}>
+                        <li className="col-md-3"><a href="#" id={s.active}>Главная</a></li>
+                        <li className="col-md-3"><a href="#">Наши волонтеры</a></li>
+                        <li className="col-md-3"><a href="#">Фотоальбом</a></li>
+                    </ul>
+                    <div className={s.signIn + " col-lg-auto"}><a href="#signIn">Войти</a></div>
+                </nav>
+                <div className={s.title + " container-md row mx-auto justify-content-between p-0"}>
+                    <div className={s.name + " col-md-5"}>
+                        <h1>Волонтерский центр <br/> "Добро = счастье" ГАПОУ TСПК</h1>
+                    </div>
+                    <div className="col-md-4"><img src={emoji} alt="emoji"/></div>
+                </div>
             </div>
-        </div>
+        </header>
     );
 };
 
