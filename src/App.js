adea263fbde6c611584c975f3538bf846d1b4bfd
Serejik <sergey.sirsilin@gmail.com>
2021-06-12 22:33:13 +0400

    fix routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react";
+import { BrowserRouter } from 'react-router-dom'
 import AppRouter from "./components/AppRouter";
 import Header from "./components/Header";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
       <AppRouter />
-    </>
+    </BrowserRouter>
   );
 }
 
