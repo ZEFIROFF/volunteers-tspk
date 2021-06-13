2b08400227cccf11047f0a072c03c7a7cb3cfac4
Денис Зефиров <55986656+d3pii@users.noreply.github.com>
2021-06-13 19:21:43 +0400

    Просто добавил рядом надпись

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from "react";
 import { BrowserRouter } from 'react-router-dom'
 import AppRouter from "./components/AppRouter";
 import Header from "./components/Header";
+import Events from "./components/Events";
 
 function App() {
   return (
     <BrowserRouter>
       <Header />
+        <Events/>
       <AppRouter />
     </BrowserRouter>
   );
--- src/components/Events/index.js
+++ src/components/Events/index.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import s from './Events.module.css'
 import Block from './BlockEvent'
+import tspk from '../../img/ТСПК.png'
 
 const MyComponent = () => {
     return (
         <div className="container-xxl mt-5">
-            <section className="container-lg p-0">
-                <h3 className="my-3">Предстоящие события</h3>
-                <Block/>
-                <Block/>
-                <div className="btnBlock mx-auto col-8">
-                    <h3 id={s.readmore}>Смотреть дальше</h3>
-                    <button type="button" className="btn btn-outline-primary">Смотреть дальше</button>
-                {/*    ебани ту которая лучше будет я сам хз прост*/}
+            <section className="container-lg m-0 p-0 row align-items-center">
+                <div className="col">
+                    <h3 className="my-3">Предстоящие события</h3>
+                    <div className={s.content}>
+                        <Block/>
+                        <Block/>
+                    </div>
+                    <div className="mx-auto col-8">
+                        <h3 id={s.readmore}>Смотреть дальше</h3>
+                    </div>
+                </div>
+                <div className="col-2">
+                    <img src={tspk} alt="ТСПК"/>
                 </div>
             </section>
         </div>
