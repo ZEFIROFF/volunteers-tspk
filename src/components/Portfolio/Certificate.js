eb86270e50687719cb7a01fa0904aa8ae78d0674
Serejik <sergey.sirsilin@gmail.com>
2021-06-19 09:44:12 +0400

    вроде доделал страничку портфолио PS адаптив? это что?

---
+++ src/components/Portfolio/Certificate.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Certificate.module.css';
+
+const Certificate = () => {
+    return (
+        <div className={`${s.certificateCard} shadow`}>
+            <p>Сертификат</p>
+        </div>
+    );
+};
+
+export default Certificate;
--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import noPhoto from '../../img/noPhoto.png'
 import s from './Portfolio.module.css';
+import Certificate from './Certificate';
 
 const Portfolio = () => {
     return (
@@ -17,7 +18,22 @@ const Portfolio = () => {
                 </div>
             </div>
             <div className="col-md-9">
-
+                <h2 className={s.portfolioTitle}>Портфолио</h2>
+                <div className={`${s.rightBlock} row`}>
+                    <div className={`${s.cardWrapper}`}>
+                        <Certificate />
+                    </div>
+                    <div className={`${s.cardWrapper}`}>
+                        <Certificate />
+                    </div>
+                    <div className={`${s.cardWrapper}`}>
+                        <Certificate />
+                    </div>
+                    <div className={`${s.cardWrapper}`}>
+                        <Certificate />
+                    </div>
+                </div>
+                <p className={s.openPortfolio}>Открыть портфолио полностью</p>
             </div>
         </div>
     );
