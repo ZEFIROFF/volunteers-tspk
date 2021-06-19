d8755f45739c4ec83c0a50d30486e6a1b80704f3
Serejik <sergey.sirsilin@gmail.com>
2021-06-19 10:08:51 +0400

    немножко поправил портфолио

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -5,35 +5,37 @@ import Certificate from './Certificate';
 
 const Portfolio = () => {
     return (
-        <div className="row container-fluid">
-            <div className={`col-md-3 ${s.leftBlock}`}>
-                <div className={s.sidebar}>
-                    <div className={s.sidebar__inner}>
-                        <div className={s.photoBlock}>
-                            <img src={noPhoto} />
+        <div className={s.container}>
+            <div className="row">
+                <div className={`col-md-3 ${s.leftBlock}`}>
+                    <div className={s.sidebar}>
+                        <div className={s.sidebar__inner}>
+                            <div className={s.photoBlock}>
+                                <img src={noPhoto} />
+                            </div>
+                            <h2 className={`${s.fullNameTitle} mt-3`}>ФИО</h2>
+                            <input type="button" value="Создать мероприятие" className={`${s.createEventButton} mt-5`} />
                         </div>
-                        <h2 className={`${s.fullNameTitle} mt-3`}>ФИО</h2>
-                        <input type="button" value="Создать мероприятие" className={`${s.createEventButton} mt-5`} />
                     </div>
                 </div>
-            </div>
-            <div className="col-md-9">
-                <h2 className={s.portfolioTitle}>Портфолио</h2>
-                <div className={`${s.rightBlock} row`}>
-                    <div className={`${s.cardWrapper}`}>
-                        <Certificate />
+                <div className="col-md-9">
+                    <h2 className={s.portfolioTitle}>Портфолио</h2>
+                    <div className={`${s.rightBlock} row`}>
+                        <div className={`${s.cardWrapper}`}>
+                            <Certificate />
+                        </div>
+                        <div className={`${s.cardWrapper}`}>
+                            <Certificate />
+                        </div>
+                        <div className={`${s.cardWrapper}`}>
+                            <Certificate />
+                        </div>
+                        <div className={`${s.cardWrapper}`}>
+                            <Certificate />
+                        </div>
                     </div>
-                    <div className={`${s.cardWrapper}`}>
-                        <Certificate />
-                    </div>
-                    <div className={`${s.cardWrapper}`}>
-                        <Certificate />
-                    </div>
-                    <div className={`${s.cardWrapper}`}>
-                        <Certificate />
-                    </div>
+                    <p className={s.openPortfolio}>Открыть портфолио полностью</p>
                 </div>
-                <p className={s.openPortfolio}>Открыть портфолио полностью</p>
             </div>
         </div>
     );
